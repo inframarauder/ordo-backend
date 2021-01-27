da4b0c92a1a3f49444843c5f45e99dc86e78e1f1
Subhasis Das <subhasisdas125@gmail.com>
2021-01-28 00:06:39 +0530

    added user model

---
+++ controllers/users.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	username: { type: String, required: true },
+	type: { type: String, enum: ["manager", "admin"], default: "manager" },
+	password: { type: String, required: true },
+});
+
+userSchema.methods.createToken = function () {
+	try {
+		const { JWT_PRIVATE_KEY } = process.env;
+		return jwt.sign({ _id: this._id, type: this.type }, JWT_PRIVATE_KEY);
+	} catch (error) {
+		console.error("Error in jwt generation\n", error);
+	}
+};
+
+userSchema.pre("save", async function (next) {
+	try {
+		if (this.isNew) {
+			const salt = await bcrypt.genSalt(12);
+			this.password = await bcrypt.hash(this.password, salt);
+		}
+
+		return next();
+	} catch (error) {
+		console.error("Error in password hashing\n", error);
+	}
+});
+
+module.exports = mongoose.model("User", userSchema);
